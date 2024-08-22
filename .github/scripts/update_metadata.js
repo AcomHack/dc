2521db49093ca79fe24a1dd5b4602babb635e23e
James Tsay <tsay@adobe.com>
2024-08-22 09:20:49 -0700

    update

--- .github/scripts/update_metadata.js
+++ .github/scripts/update_metadata.js
@@ -1,11 +1,17 @@
 async function main() {
+  let url = process.argv[2];
+
+  if (!url.endsWith('.md')) return;
+
+  url = url.replace('.md', '');
+
   fetch(process.env.UPDATE_METADATA_WEBHOOK, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-      URL: "/drafts/tsay/pdf-editor-pdf-files",
+      URL: url,
       Preloads: "$MILOLIBS/martech/martech.js,$MILOLIBS/blocks/marquee/marquee.js,$MILOLIBS/blocks/marquee/marquee.css,$MILOLIBS/utils/decorate.js,$MILOLIBS/utils/lana.js,$MILOLIBS/utils/samplerum.js,$MILOLIBS/features/personalization/personalization.js"
     }),
   }).then((response) => {
